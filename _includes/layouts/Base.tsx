e9d80e81737514d504e815c951d788c845bb9436
ansanloms <ansanloms@users.noreply.github.com>
2022-09-24 17:26:35 +0900

    fix

--- _includes/layouts/Base.tsx
+++ _includes/layouts/Base.tsx
@@ -13,6 +13,14 @@ export default (props: Props) => (
         content="width=device-width, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no"
       />
       <title>{props.title}</title>
+      <link
+        rel="stylesheet"
+        href="https://cdn.jsdelivr.net/npm/highlight.js@11.6.0/styles/obsidian.css"
+      />
+      <link
+        rel="stylesheet"
+        href="https://cdn.jsdelivr.net/npm/ress@5.0.2/dist/ress.min.css"
+      />
       <link rel="stylesheet" href="/styles/main.css" />
       <link
         rel="stylesheet"
