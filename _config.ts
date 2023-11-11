daec21a85f402a2f3df99ae73476e039845b08f0
ansanloms <ansanloms@users.noreply.github.com>
2023-11-12 00:28:50 +0900

    fix: PlantUML プラグインの置換。

--- _config.ts
+++ _config.ts
@@ -5,7 +5,7 @@ import jsx from "lume/plugins/jsx_preact.ts";
 import sass from "lume/plugins/sass.ts";
 import metas from "lume/plugins/metas.ts";
 import codeHighlight from "lume/plugins/code_highlight.ts";
-import plantuml from "./_includes/plugins/plantuml.ts";
+import plantuml from "lume-plugin-plantuml/mod.ts";
 import mermaid from "./_includes/plugins/mermaid.ts";
 
 const site = lume({
@@ -19,7 +19,14 @@ site.use(jsx());
 site.use(sass());
 site.use(metas());
 site.use(codeHighlight());
-site.use(plantuml());
+site.use(plantuml({
+  binary: {
+    version: "v1.2023.6",
+    dest: "./_bin/plantuml.jar",
+    checksum:
+      "bf2dee10750fd1794ad9eac7de020064d113838ec169448a16b639dbfb67617d",
+  },
+}));
 site.use(mermaid());
 
 site.copy("assets");
