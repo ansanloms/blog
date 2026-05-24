9b369d467b7a45ecb713b6cc849cbd99a8fe69e0
ansanloms <16961622+ansanloms@users.noreply.github.com>
2026-05-24 22:55:36 +0900

    fix: _config.ts

--- _config.ts
+++ _config.ts
@@ -1,5 +1,6 @@
 import lume from "lume/mod.ts";
 import blog from "lume-theme-blog/mod.ts";
+import basePath from "lume/plugins/base_path.ts";
 
 const site = lume({
   src: "./docs",
@@ -7,13 +8,29 @@ const site = lume({
 });
 
 site
+  .copy([".jpg", ".jpeg", ".gif", ".png", ".svg", ".webp", ".csv"])
+  .use(basePath())
   .use(blog({
+    /**
+     * markdown の設定。
+     */
     markdown: {
+      /**
+       * シンタックスハイライトの設定。
+       */
       syntaxHighlight: {
-        themes: { light: "nord", dark: "nord" },
+        /**
+         * シンタックスハイライトのテーマ。
+         * Shiki(https://shiki.style/)のテーマを指定する。
+         */
+        themes: { light: "github-light", dark: "github-dark" },
+
+        /**
+         * ハイライト対象外の言語。
+         */
+        exclude: ["mermaid"],
       },
     },
-  }))
-  .copy("assets");
+  }));
 
 export default site;
